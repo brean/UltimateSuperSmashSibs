677dda4076d198c587ffc33277eb017efa5ad8c2
Andreas Bresser <self@andreasbresser.de>
2019-01-26 20:25:37 +0100

    Player select screen (WIP)

---
+++ Assets/Scripts/Manager/PlayerSelect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    public GameObject PlayerSelectPrefab;
+    Dictionary<string, GameObject> Player = new Dictionary<string, GameObject>();
+
+    void CreatePlayer(string inputName)
+    {
+        if (Player.ContainsKey(inputName))
+        {
+            return;
+        }
+        GameObject NextPlayer = Instantiate(PlayerPrefab);
+        NextPlayer.GetComponent<InputControl>().inputName = inputName;
+        NextPlayer.GetComponent<PlayerTeam>().SetTeam(Player.Count % 2);
+        // TODO: team = Player.Count % 2
+        Player.Add(inputName, NextPlayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.UpArrow) ||
+            Input.GetKeyDown(KeyCode.DownArrow) ||
+            Input.GetKeyDown(KeyCode.LeftArrow) ||
+            Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            CreatePlayer("ArrowKeys");
+        }
+        if ((Input.GetKeyDown(KeyCode.W) ||
+            Input.GetKeyDown(KeyCode.S) ||
+            Input.GetKeyDown(KeyCode.A) ||
+            Input.GetKeyDown(KeyCode.D)))
+        {
+            CreatePlayer("WasdKeys");
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetAxis("Joy" + i + "Horizontal") > .2 ||
+                Input.GetAxis("Joy" + i + "Horizontal") > .2)
+            {
+                CreatePlayer("Joy" + i);
+            }
+        }
+    }
+}
