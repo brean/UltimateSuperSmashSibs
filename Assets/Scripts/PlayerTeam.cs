90948a5ec73fb7265e9145cf7b1de4de446f7e72
Andreas Bresser <self@andreasbresser.de>
2019-01-26 10:16:59 +0100

    Player Team

---
+++ Assets/Scripts/PlayerTeam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeam : MonoBehaviour
+{
+    Color[] colors = { Color.blue, Color.red };
+    public int teamnr;
+    // Start is called before the first frame update
+    public void SetTeam(int nr)
+    {
+        teamnr = nr;
+        Color col = colors[nr];
+        GameObject.Find("Top").GetComponent<SpriteRenderer>().color = new Color(col.r, col.g, col.b);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
