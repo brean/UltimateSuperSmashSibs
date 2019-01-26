aeeba75b1cf7f33b3dd88a821f8973e02d9bcaa0
mb <murks@batr.am>
2019-01-26 18:04:18 +0100

    use box coliders for camera borders, change player names in TilesetTest

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -17,6 +17,7 @@ public class PlayerManager : MonoBehaviour
     public float xPlayerMin = 0;
 
     public float camHalfWidth = 0;
+    public float mapBorderOffset = 0.5f;
 
     public Dictionary<string, GameObject> PlayerDict = new Dictionary<string, GameObject>();
     GameObject camera;
@@ -25,15 +26,28 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         //hardcoded test players
-        PlayerDict.Add("moep1", GameObject.Find("Player 1"));
-        PlayerDict.Add("moep2", GameObject.Find("Player 2"));
-        PlayerDict.Add("moep3", GameObject.Find("Player 3"));
-        PlayerDict.Add("moep4", GameObject.Find("Player 4"));
+        PlayerDict.Add("moep1", GameObject.Find("Player1"));
+        PlayerDict.Add("moep2", GameObject.Find("Player2"));
+        PlayerDict.Add("moep3", GameObject.Find("Player3"));
+        PlayerDict.Add("moep4", GameObject.Find("Player4"));
 
         Camera cam = Camera.main;
         camera = Camera.main.gameObject;
-
         camHalfWidth = ((2f * cam.orthographicSize) * cam.aspect) / 2f;
+
+        camera.AddComponent<BoxCollider2D>();
+        camera.AddComponent<BoxCollider2D>();
+
+        BoxCollider2D[] colliders = camera.GetComponents<BoxCollider2D>();
+        if (colliders.Length == 2)
+        {
+            //start
+            colliders[0].offset = new Vector2(-camHalfWidth + mapBorderOffset, 0);
+            colliders[0].size = new Vector2(mapBorderOffset, 100);
+            //end
+            colliders[1].offset = new Vector2(camHalfWidth - mapBorderOffset, 0);
+            colliders[1].size = new Vector2(mapBorderOffset, 100);
+        }
 
     }
 
@@ -92,7 +106,7 @@ public class PlayerManager : MonoBehaviour
         // camera is always in the middle of all players
         // camera ignores padding
         float newCamPosX = xPlayerMin + ((xPlayerMax - xPlayerMin) / 2);
-        if (newCamPosX < (camHalfWidth))
+        if (newCamPosX < camHalfWidth)
         {
             camera.transform.position = new Vector3(camHalfWidth, camera.transform.position.y, camera.transform.position.z);
         }
